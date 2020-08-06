7c9719a9301280a8f30d816f58845b413e9d8b52
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-06 11:04:35 +0300

    Initial Commit

---
+++ PreviewConfiguration/PreviewConfigurationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewConfigurationApp.swift
+//  PreviewConfiguration
+//
+//  Created by ramil on 06.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct PreviewConfigurationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
